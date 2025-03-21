ee2c95d30217d24110c2842d28179f2cf1c90503
Icaro Novais <icaronovais9@gmail.com>
2025-03-21 00:18:15 -0300

    aula11

---
+++ aula11-condições/ex008.js
@@ -0,0 +1,7 @@
+console.log('O console funcionou corretamente!')
+var vel = 120
+console.log(`A velociade do seu carro é ${vel}Km/h`)
+if (vel > 60){ // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
